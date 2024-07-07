9b3c5ce29b1e4929e33a1cb150ab44d0e6531366
Yatharth <yatharth_tomar@srmap.edu.in>
2024-07-07 11:08:20 +0530

    Basics of Mat datatype added

--- OpenCV-Core Foundation/OpenCV-Core Foundation.cpp
+++ OpenCV-Core Foundation/OpenCV-Core Foundation.cpp
@@ -1,20 +1,82 @@
-// OpenCV-Core Foundation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//opencv Mat data type
 
-#include <iostream>
+//Mat the basic container
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//Creating Mat Object explicitly"
+//8UC3 -> 8 bit Unsigned character, 3 channels
+
+/*
+Data Type :
+
+8U: 8 - bit unsigned integers(0 - 255).
+8S : 8 - bit signed integers(-128 to 127).
+16U : 16 - bit unsigned integers(0 - 65535).
+16S : 16 - bit signed integers(-32768 to 32767).
+32S : 32 - bit signed integers.
+32F : 32 - bit floating - point numbers.
+64F : 64 - bit floating - point numbers.
+
+*/
+
+#include <opencv2/opencv.hpp> 
+#include<iostream>
+using namespace std;
+using namespace cv;
+
+
+int main() {
+
+	//creating Mat explicitly using Mat constructor
+	Mat M(2, 2, CV_16UC3, Scalar(0, 0,255));
+	cout << M << endl << endl;
+
+	Mat E = Mat::eye(4, 4, CV_64F);
+	cout << endl << "E= " << endl << E << endl << endl;
+	Mat O = Mat::ones(6, 6, CV_16U);
+	cout << endl << "O= " << endl << O << endl << endl;
+	Mat Z = Mat::zeros(3, 3, CV_8UC1);
+	cout <<endl<< "Z = " << endl << " " << Z << endl << endl;
+
+
+	//create headr part of Mat
+	Mat A, C;
+	A = imread("./assets/basket.jpg");
+	//copy constructor
+	Mat B(A);
+	C = A;
+
+	//cout << endl << "A = " << A << endl << endl;
+	//all the above methods point to the same single data mtrix and making a 
+	//modification using any of them will affect all the other one as well.
+
+
+	//In case, if we want to create the copy of matrix itself too
+
+	Mat F = A.clone();
+	Mat G;
+	A.copyTo(G);
+	//cout << endl << "G = " << G << endl << endl;
+
+	//If we want to refer to only asubsection of full data
+
+
+	//Range of Interest 
+
+	Mat X(A, Rect(10, 10, 100, 100));//x,y-10,10  width,height-100,100
+	//cout << endl << "X = " << X << endl << endl;
+
+	//we can also use row and column boudaries
+	/*Range(0, 2) specifies the range of columns to be included in S.
+0: The starting column index (inclusive).
+2: The ending column index (exclusive).*/
+	cout << "Number of rows in image : "<<A.rows<<endl;
+	cout << "NUmber of columns in image are : " << A.cols << endl;
+	Mat S = A(Range::all(), Range(0, 900));
+	//cout << endl << "S = " << S << endl << endl;
+
+	imshow("row and column",S);
+	waitKey(0);
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
